ee42a1eef1da5f03832efd1acd343e0beafc77e2
WinterXMQ <WinterXMQ@gmail.com>
2014-05-11 00:56:06 +0800

    Fix V4 vaild <- CheckSignature

--- index.php
+++ index.php
@@ -1,25 +1,65 @@
 <?php
-/*
-    WinterXMQ <WinterXMQ@gmail.com>
-    Version 1.0.0.1
-*/
-define("TOKEN", "weixin");
-$wmCharObj = new WMChartCallBackAPI();
-$wmCharObj->valid();
+/**
+  * wechat php test
+  */
 
-class WMChartCallBackAPI {
-    /**
-     *  微信平台回调，监测双方合理权限
-     */
-    public function valid() {
-        $echoStr = $_GET["echostr"];    // 监测通过后用于返回, 便于微信平台判断
-        if (this->CheckSignature()) {
+//define your token
+define("TOKEN", "weixin");
+$wechatObj = new wechatCallbackapiTest();
+$wechatObj->valid();
+
+class wechatCallbackapiTest
+{
+    public function valid()
+    {
+        $echoStr = $_GET["echostr"];
+
+        //valid signature , option
+        if($this->checkSignature()){
             echo $echoStr;
             exit;
         }
     }
 
-    private function CheckSignature() {
+    public function responseMsg()
+    {
+        //get post data, May be due to the different environments
+        $postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
+
+        //extract post data
+        if (!empty($postStr)){
+                
+                $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
+                $fromUsername = $postObj->FromUserName;
+                $toUsername = $postObj->ToUserName;
+                $keyword = trim($postObj->Content);
+                $time = time();
+                $textTpl = "<xml>
+                            <ToUserName><![CDATA[%s]]></ToUserName>
+                            <FromUserName><![CDATA[%s]]></FromUserName>
+                            <CreateTime>%s</CreateTime>
+                            <MsgType><![CDATA[%s]]></MsgType>
+                            <Content><![CDATA[%s]]></Content>
+                            <FuncFlag>0</FuncFlag>
+                            </xml>";             
+                if(!empty( $keyword ))
+                {
+                    $msgType = "text";
+                    $contentStr = "Welcome to wechat world!";
+                    $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+                    echo $resultStr;
+                }else{
+                    echo "Input something...";
+                }
+
+        }else {
+            echo "";
+            exit;
+        }
+    }
+        
+    private function checkSignature()
+    {
         $signature = $_GET["signature"];
         $timestamp = $_GET["timestamp"];
         $nonce = $_GET["nonce"];    
@@ -32,10 +72,9 @@ class WMChartCallBackAPI {
         
         if( $tmpStr == $signature ){
             return true;
-        } else {
+        }else{
             return false;
         }
-    }
     }
 }
 
