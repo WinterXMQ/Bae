9ac7627d0e506c4afe154b81356d35299e80df60
WinterXMQ <WinterXMQ@gmail.com>
2014-05-11 01:09:45 +0800

    Last WM Test

--- index.php
+++ index.php
@@ -1,36 +1,49 @@
 <?php
-define ( "TOKEN", "dismath" );
-$wechatObj = new wechatCallbackapiTest();
-$wechatObj->valid();
+/**
+ *   WinterXMQ <WinterXMQ@gmail.com>
+ *   Version 1.0.0.1
+ */
+define ( "TOKEN", "WinterXMQ" );
 
-class wechatCallbackapiTest
-{
-    public function valid()
-    {
+
+$weChatObj = new WeChatCallbackAPI();
+$weChatObj->valid();
+
+class WeChatCallbackAPI {
+    /**
+     * 用于微信平台和回复平台交流
+     */
+    public function valid() {
         $echoStr = $_GET["echostr"];        //随机字符串
         if($this->checkSignature()){
             echo $echoStr;
             exit;
         }
     }
+
+    /**
+     * 检查是否是微信平台的数据访问
+     */
     private function checkSignature() {
-      $signature = $_GET ["signature"];
-      $timestamp = $_GET ["timestamp"];
-      $nonce = $_GET ["nonce"];
-      $token = TOKEN;
-      $tmpArr = array (
+
+        $signature = $_GET ["signature"];
+        $timestamp = $_GET ["timestamp"];
+        $nonce = $_GET ["nonce"];
+
+        $token = TOKEN;
+        $tmpArr = array (
             $token,
             $timestamp,
             $nonce
-      );
-      sort ( $tmpArr );
-      $tmpStr = implode ( $tmpArr );
-      $tmpStr = sha1 ( $tmpStr );
-      
-      if ($tmpStr == $signature) {
-        return true;
-      } else {
-        return false;
-      }
+        );
+        sort ( $tmpArr );
+        $tmpStr = implode ( $tmpArr );
+        $tmpStr = sha1 ( $tmpStr );
+        
+        if ($tmpStr == $signature) {
+            return true;
+        } else {
+            return false;
+        }
     }   
 }  
